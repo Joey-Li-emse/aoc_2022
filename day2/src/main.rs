9bd64aee6c7658986b2e2d1b8cddc360a9d4347b
Joey Li <joey@MacBook-Pro-de-Joey.local>
2023-03-02 22:29:29 +0100

    day 2 clippy

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,22 +1,13 @@
 use std::fs; 
 
-const RK : u8 = 1;
-const PP : u8 = 2;
-const SC : u8 = 3; 
+const RK : u32 = 1;
+const PP : u32 = 2;
+const SC : u32 = 3; 
 
-const WIN : u8 = 6; 
-const LOS : u8 = 0; 
-const DRW : u8 = 3; 
+const WIN : u32 = 6; 
+const LOS : u32 = 0; 
+const DRW : u32 = 3; 
 
-enum Winner{
-    Me,
-    Elf, 
-}
-enum Outcome {
-    Win,
-    Lose,
-    Draw,
-}
 
 #[derive(PartialEq, Debug)]
 enum Play{
@@ -59,48 +50,37 @@ fn main() {
 
 }
 
-// fn who_win(duel : (Play, Play)) -> Outcome{
-//     if duel.0 == duel.1
-//     {
-//         return Outcome::Draw
-//     }
-//     else if ( (duel.0 == Play::Paper && duel.1 == Play::Scissor) ||
-//               (duel.0 == Play::Rock && duel.1 == Play::Paper) || 
-//               (duel.0 == Play::Scissor && duel.1 == Play::Rock)){
-//         return Outcome::Win
-//     }
-//     return Outcome::Lose
-// }
+
 
 fn who_win(duel : (Play, u32)) -> u32{
     if        (duel.0 == Play::Paper && duel.1 == 2) ||
               (duel.0 == Play::Rock && duel.1 == 1) || 
               (duel.0 == Play::Scissor && duel.1 == 3){
-        return 3
+        return DRW
     }
     else if   (duel.0 == Play::Paper && duel.1 == 3) ||
               (duel.0 == Play::Rock && duel.1 == 2) || 
               (duel.0 == Play::Scissor && duel.1 == 1){
-        return 6
+        return WIN
     }
-    return 0
+    LOS
 }
 
 fn to_win(npc : &Play, outcome : u32) -> u32{
     if  (npc == &Play::Paper && outcome == 6)||
         (npc == &Play::Rock && outcome == 0)||
         (npc == &Play::Scissor && outcome == 3){
-            return 3
+            SC
         }
     else if  (npc == &Play::Paper && outcome == 0)||
     (npc == &Play::Rock && outcome == 3)||
     (npc == &Play::Scissor && outcome == 6){
-        return 1
+        return RK
     }
     else if  (npc == &Play::Paper && outcome == 3)||
     (npc == &Play::Rock && outcome == 6)||
     (npc == &Play::Scissor && outcome == 0){
-        return 2
+        return PP
     }
     else{
         panic!("shouldn'tbe happening");
@@ -110,22 +90,13 @@ fn to_win(npc : &Play, outcome : u32) -> u32{
 
 fn outcome(out: &str) -> u32{
     match out{
-        "X" => 0,
-        "Y" => 3,
-        "Z" => 6,
+        "X" => LOS,
+        "Y" => DRW,
+        "Z" => WIN,
         _ => 0, 
     }
 }
 
-// fn played(npc: &str, player: &str) -> (Play, Play)
-// {
-//     (npc_convert(npc), pla_convert(player))
-// }
-
-fn played(npc: &str, player: &str) -> (Play, u32)
-{
-    (npc_convert(npc), pla_convert(player))
-}
 
 fn npc_convert(play : &str) -> Play
 {
@@ -137,22 +108,12 @@ fn npc_convert(play : &str) -> Play
     }
 }
 
-// fn pla_convert(play : &str) -> Play
-// {
-//     match play{
-//         "X" => Play::Rock,
-//         "Y" => Play::Paper,
-//         "Z" => Play::Scissor, 
-//         _   => Play::Unknown,
-//     }
-// }
-
 fn pla_convert(play : &str) -> u32
 {
     match play{
-        "X" => 1,
-        "Y" => 2,
-        "Z" => 3, 
+        "X" => RK,
+        "Y" => PP,
+        "Z" => SC, 
         _   => 0,
     }
 }
@@ -167,12 +128,5 @@ mod tests
     {   
         let play = npc_convert("A");
         assert_eq!(play, Play::Rock); 
-    }
-
-    #[test]
-    fn duel_test()
-    {
-        let duel = played("A", "Z");
-        assert_eq!(duel, (Play::Rock, 3));
     }
 }
---
+++ day2/target/debug/deps/day2-cc3a2656c04d374e.d
@@ -0,0 +1,8 @@
+/Users/joey/Documents/Rust/aoc_2022/day2/target/debug/deps/day2-cc3a2656c04d374e.rmeta: src/main.rs Cargo.toml
+
+/Users/joey/Documents/Rust/aoc_2022/day2/target/debug/deps/day2-cc3a2656c04d374e.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
