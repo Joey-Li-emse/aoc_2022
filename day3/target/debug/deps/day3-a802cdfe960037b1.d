60d23bd1682e935100089f76a1bebcfc5be2d08c
Joey Li <joey@mbp-de-joey.home>
2023-03-03 14:39:06 +0100

    day 3 init

---
+++ day3/target/debug/deps/day3-a802cdfe960037b1.d
@@ -0,0 +1,5 @@
+/Users/joey/Documents/Rust/aoc_2022/day3/target/debug/deps/day3-a802cdfe960037b1.rmeta: src/main.rs
+
+/Users/joey/Documents/Rust/aoc_2022/day3/target/debug/deps/day3-a802cdfe960037b1.d: src/main.rs
+
+src/main.rs:
---
+++ day3/target/debug/deps/day3-f157d7b99458491d.d
@@ -0,0 +1,5 @@
+/Users/joey/Documents/Rust/aoc_2022/day3/target/debug/deps/day3-f157d7b99458491d.rmeta: src/main.rs
+
+/Users/joey/Documents/Rust/aoc_2022/day3/target/debug/deps/day3-f157d7b99458491d.d: src/main.rs
+
+src/main.rs:
